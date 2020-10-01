f1faacde526690814fe00dc67b76fee8925bb6fa
xPolar <harismehrzad12@gmail.com>
2020-10-01 00:46:03 -0700

    Redo GuessGame.py Remove math import as it's not used anywhere within the file. Add more descriptive comments. Better grammar throughout the file. Less lines as well as inline if statements used.

--- Guess_Game.py
+++ Guess_Game.py
@@ -1,46 +1,48 @@
+# Imports.
 import random
-import math
-#Welcome Message
-print("Welcome to the Guess Game")
 
-#Getting Player Name and options to play
-user_name = input("Enter your name : ")
+# Welcome message.
+print("Welcome to the guessing game!")
 
-#Getting Choices
-print(user_name + " enter the choice between which you wish to guess : ")
-print("You have got infinite chances, chill and  all the best !")
-print("Option A: 10-20","Option B: 20-30", "Option C: 30-40", "Option D: 40-50", sep = '\n')
-#Setting Guess counter
-def guess_number(user1):
-    guesses = 0
-    user1=user1.upper()
-    if user1== "A":
-        answer_user1 = random.randint(10, 21)
-    elif user1== "B":
-        answer_user1 = random.randint(20, 31)
-    elif user1== "C":
-        answer_user1 = random.randint(30, 41)
-    elif user1== "D":
-        answer_user1 = random.randint(40, 51)
-    while guesses < 5:
-        user_input = input("Enter your Guess : ")
-        try:
-            user_input=int(user_input)
-            if user_input == answer_user1:
-                print("Congratulations " + user_name + " you did it.")
-                break
-            elif user_input > answer_user1:
-                print("Oh! You guessed higher, try again!")
-            elif user_input < answer_user1:
-                print("Oh! You guessed lower, try again!")
-            guesses=guesses+1
-            print(f"No of guesses left {5-guesses}")
-        except ValueError:
-            print("Pls Input Intergers only! TRY AGAIN")
-            continue
-    if guesses>=5:
-        print("Game Over")
+# Get player's name.
+name = input("What is your name?: ")
 
+# Set minimum and maximum to None to allow us to perform a for loop.
+minimum = None
+maximum = None
 
-choice_input = input("Enter your Choice : ")
-guess_number(choice_input)
+# Ask the user to enter a minimum and a maximum number.
+## One would think that it would be better to have both inputs in the same while loop however if the user
+## gets the maximum number wrong they would have to renter the minimum number then do the maximum number again.
+while minimum == None: 
+    try:
+        minimum = int(input("Please enter a minimum number: "))
+    except ValueError:
+        print("Please provide a numeric number!")
+while maximum == None:
+    try:
+        maximum = int(input("Please enter a maximum number: "))
+    except ValueError:
+        print("Please provide a numeric number!")
+
+    # If the minimum number is greater then the maximum ask the user to enter a new maximum.
+    if maximum < minimum:
+        print(f"Please provide a number greater than {minimum}!")
+        maximum = None
+
+# Generate the number by doing randint.
+number = random.randint(minimum, maximum)
+
+while True:
+
+    # If the guess isn't a number ask them to provide a number.
+    try:
+        guess = int(input("Please provide a guess: "))
+    except ValueError:
+        print("Please provide a numeric number!")
+
+    # If the guess is the number let them know, otherwise tell them if they're too hugh or too low.
+    if guess == number:
+        print(f"Nice job {name}, you guessed the right number!")
+        break
+    print("Too low!" if guess < number else "Too high!")
