54cf0a4a163b4f3e7bf4a5651ce002d89b945847
7h3r4v3n <49156472+7h3r4v3n@users.noreply.github.com>
2020-10-06 20:53:27 +0530

    Diagonal_Difference_for_a_matrix

---
+++ python/Diagonal_Difference_for_a_matrix.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    diagonal1 = 0
+    diagonal2 = 0
+    for i in range(len(arr)):
+        diagonal1 += arr[i][i]
+        diagonal2 += arr[i][len(arr)-i-1]
+    return(abs(diagonal1- diagonal2))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
