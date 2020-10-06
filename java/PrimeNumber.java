5ff43f1794ea5bac8bffe4aca3bf72850d511899
lucaskarlsson <Lucaskarlsson@live.se>
2020-10-06 19:00:41 +0200

    Changed PrimeNumber in java so it's actually working

--- java/PrimeNumber.java
+++ java/PrimeNumber.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
 
 public class PrimeNumber {
-
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);	
-		System.out.print("Enter Number here: ");
-		int n = sc.nextInt();
-		CheckPrimeNumber(n);
+		new PrimeNumber().program();
 	}
 
-	public static void CheckPrimeNumber(int n) {
+	public void program() {
+		Scanner sc = new Scanner(System.in);
+		int n;
 		boolean isPrime = true;
-		if(n<2) { isPrime = false;}
-		for(int i = 2; i < n; i++) {
-			if(n % i ==0) {
-				isPrime = false;
-				System.out.println("Number is not Prime");
-				break;
-			}else {
-				System.out.println("Number is Prime");
-				break;
+		while (isPrime) {
+			System.out.print("Enter number: ");
+			n = sc.nextInt();
+
+			isPrime = checkPrime(n);
+			System.out.println(isPrime);
+		}
+	}
+
+	public boolean checkPrime(int n) {
+		if (n < 2) {
+			return false;
+		}
+
+		for (int i = 2; i<n; i++) {
+			if (n%i == 0) {
+				return false;
 			}
 		}
+		return true;
 	}
 }
