adf5220235a666a3b6e2ddcd3f663cea983c07ff
David del Río Medina <ddrm86@gmail.com>
2020-10-08 12:52:41 +0200

    Issue #183 adding custom swipe probabilities

--- python/TinderBot/Bot.py
+++ python/TinderBot/Bot.py
@@ -8,14 +8,20 @@ print("****Tinder swiper V2****")
 pyautogui.FAILSAFE = False
 print('Make sure you are already logged in Tinder On your Browser')
 a = int(input('How many people do you want to swipe?\n'))
+percent_right = int(input('Probabilities of swiping right? [0-100]\n'))
+probs_right = percent_right / 100.0
 
-webbrowser.open('https://www.tinder.com',new = 1,autoraise = True)
+webbrowser.open('https://www.tinder.com', new=1, autoraise=True)
 time.sleep(10)
 
-for i in range(0,a) :
-	if random.randint(0,1) > 0.5:
+for i in range(0, a):
+	r = random.random()
+	swipe_direction = None
+	if r < probs_right:
+		swipe_direction = 'right'
 		pyautogui.press('right')
 	else:
+		swipe_direction = 'left'
 		pyautogui.press('left')
 	time.sleep(1)
-	print('person',i+1,'swiped')
+	print('person', i+1, 'swiped', swipe_direction)
