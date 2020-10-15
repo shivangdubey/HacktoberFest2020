8fa62743f5e9934a0d11432968e919cd02a6dfca
Arpit1000 <arpit95agarwal@gmail.com>
2020-10-16 02:16:36 +0530

    made fibonaaci

--- c/fibanocci.c
+++ c/fibanocci.c
@@ -5,12 +5,25 @@ int main() {
     scanf("%d", &n);
     printf("Fibonacci Series: ");
 
-    for (i = 1; i <= n; ++i) {
+
+    if(n==1)
+        printf("%d, ", t1);
+    else if(n==2)
+    {
+        printf("%d ",t1 );
+        printf("%d, ", t2);
+    }
+    else
+    {
+       for (i = 1; i <= n; ++i) {
         printf("%d, ", t1);
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
     }
 
-    return 0;
+}
+
+
+return 0;
 }
