3cda638a405cca604fc109cb552d9fb559ac3cc9
Ananya Sharma <Ananya Sharma>
2020-10-17 18:32:30 +0530

    done some changes in string scramble

--- LeetCode/Python/string_scramble.py
+++ LeetCode/Python/string_scramble.py
@@ -1,11 +1,10 @@
 def StringScramble(str1,str2):
-
   list1 = set(list(str1))
   list2 = set(list(str2))
   list3 = list1.intersection(list2)
-  s = "".join(sorted(list3))
-  s1 = "".join(sorted(list2))
-  if s == s1:
+  string1 = "".join(sorted(list3))
+  string2 = "".join(sorted(list2))
+  if string1 == string2:
     return "true"
   else:
     return "false"
