8221bb2085e4ea3a7280c56abdcecf966d28b198
Anwesha1308 <68744661+Anwesha1308@users.noreply.github.com>
2020-10-17 10:28:44 +0530

    Updated Fibonacci.py
    Updated fibonacci.py to make it more user friendly.
    Added a while loop and try, except statement to print an error message for invalid inputs and continue accepting inputs until the user enters an integer.

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,10 +1,19 @@
-num = int(input("Enter the Number:"))
+#I've included while loop and try except so that invalid inputs can be detected and error message printed
+while True:
+    num = input("Enter the Number:")
+    try:
+        n = int(num)
+        break
+    except:
+        print("Invalid Input. You can enter only positive integers.")
 n1, n2 = 0, 1
-print("Fibonacci Series:", n1, n2, end=",")
-for i in range(2, num):
+#In the following line there was a slight mistake, it was written end=','
+#I replaced it with end=" " so that it remains consistent with line 17 of the code
+print("Fibonacci Series:", n1, n2, end=" ")
+for i in range(2, n):
     n3 = n1 + n2
     n1 = n2
     n2 = n3
     print(n3, end=" ")
 
-print()
+#Here I have omitted print() because that was unnecessary.
