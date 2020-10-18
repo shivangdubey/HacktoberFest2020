2e94c792074486978f6aba3c30b93f2f53c433bc
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2020-10-18 13:13:51 +0530

    updated a bit

--- python/Count the Occurrences of a Word in a Text File.py
+++ python/Count the Occurrences of a Word in a Text File.py
@@ -1,12 +1,13 @@
 fname = input("Enter file name: ")
-word=input("Enter word to be searched:")
+word = input("Enter word to be searched:")
 k = 0
  
 with open(fname, 'r') as f:
+    f = f.lower()
     for line in f:
         words = line.split()
         for i in words:
             if(i==word):
                 k=k+1
-print("Occurrences of the word:")
+print("Occurrences of the word : ", end = "")
 print(k)
