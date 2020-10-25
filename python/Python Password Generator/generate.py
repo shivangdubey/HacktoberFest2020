99777056bccd139578cfba5cc8f117b3eecc53b7
Seema Saharan <48756444+seema1711@users.noreply.github.com>
2020-10-25 19:35:19 +0530

    Python Password Generator

--- python/Python Password Generator/generate.py
+++ python/Python Password Generator/generate.py
@@ -2,4 +2,4 @@ import string
 from random import *
 char = string.ascii_letters + string.punctuation + string.digits 
 passwordGenerator = "".join(choice(char) for x in range(randint(8,16)))
-print(passwordGenerator)
+print(passwordGenerator
