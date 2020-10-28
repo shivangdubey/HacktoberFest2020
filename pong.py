21df6a2052c27b360d66a521e609fb7ed8540eaf
utkxxrsh <64093818+utkxxrsh@users.noreply.github.com>
2020-10-28 22:24:19 +0530

    Pong Game Commited

---
+++ pong.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+        
+
+        
+    
+    
+    
+    
+i=input()
+x=len(i)
+s=""
+d=""
+for z in range(0,x):
+    y=i[z]
+    if y=="a":
+        s=s+y
+    elif y=="e":
+        s=s+y
+    elif y=="i":
+        s=s+y
+    elif y=="o":
+        s=s+y
+    elif y=="u":
+        s=s+y
+    else:
+        d=d+y
+print(s)
+print(d)
+
+    
+    
+        
+    
+    
+    
+        
+    
+
