bd9f2cb17e7020da67aa6c04f5b274ab6b4bfe06
ArshlanSiddique <57309295+ArshlanSiddique@users.noreply.github.com>
2020-10-30 23:54:13 +0530

    Create Number Tables Creator.c
    code to Create Numbers Table

---
+++ c/Number Tables Creator.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+  //code to Create Numbers Table
+  int main()
+   {
+       int a,b;
+       printf("Enter a number here-");
+       scanf("%d", &a);
+       printf("The Table of %d\n",a);
+       printf("%dx1=%d\n",a,a*1,a);
+       printf("%dx2=%d\n",a,a*2,a);
+       printf("%dx3=%d\n",a,a*3,a);
+       printf("%dx4=%d\n",a,a*4,a);
+       printf("%dx5=%d\n",a,a*5,a);
+       printf("%dx6=%d\n",a,a*6,a);
+       printf("%dx7=%d\n",a,a*7,a);
+       printf("%dx8=%d\n",a,a*8,a);
+       printf("%dx9=%d\n",a,a*9,a);
+       printf("%dx10=%d\n",a,a*10,a);
+  return 0;
+}
