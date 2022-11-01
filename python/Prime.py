e50144b3016c9a0af0400d1907f80e90b4763728
__anuragdaksh7__ <84393491+anuragdaksh7@users.noreply.github.com>
2022-11-01 22:57:17 +0530

    Update Prime.py
    I think that since we have to check prime numbers in a big range so optimizing the code should be priority else it would take hours to check for some very big value of "number"

--- python/Prime.py
+++ python/Prime.py
@@ -1,14 +1,19 @@
 def is_prime(a):
-    flag = True
-    for i in range(2,a//2+1):
-        if(a%i == 0):
-            flag = False
-            break
-    return flag
+    if type(a) != int:
+        return False
+    if a<2:
+        return False
+    if a==2:
+        return True
+    if a%2==0:
+        return False
+    for i in range(3,int(a**0.5)+1,2):
+        if a%i==0:
+            return False
+    return True
+
 number = int(input('Enter a Number to which You want to find Prime Numbers : '))
-if(number < 2):
-    print('No Prime numbers < 2')
-else:
-    for i in range(2, number):
-        if(is_prime(i)):
-            print(i)
+
+for i in range(2, number):
+    if(is_prime(i)):
+        print(i)
